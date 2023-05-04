d8482fa488ec0485645b630be27bb0c64dec06c4
Richard <richard@test.com>
2023-05-04 02:04:16 -0300

    Starting the practical exam

---
+++ src/test/java/com/example/PracticalTest/PracticalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
